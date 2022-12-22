3989f407924844cda8316bc5a7a2db426fd6f1e1
EduardoBraz1 <esb1106@gmail.com>
2022-12-22 16:33:02 -0300

    adicionando exercicios javascript

--- 7 avaliação/3_exercicio.js
+++ 7 avaliação/3_exercicio.js
@@ -1,6 +1,6 @@
-/* 3) Faça um programa que calcule e imprima o salário salário a ser transferido para um funcionário.
+/* 3) Faça um programa que calcule e imprima o salário a ser transferido para um funcionário.
 Para realizar o calculo receba o valor bruto do salário e o adicional dos benefícios.
-O salário salário a ser transferido é calculado da seguinte maneira: 
+O salário a ser transferido é calculado da seguinte maneira: 
    
    valor bruto do salário - percentual de imposto mediante a faixa salarial + adicional dos benefícios
 
@@ -18,3 +18,31 @@ Para calcular o percentual de imposto segue as aliquotas:
         Saída:
             2050.00
 */
+
+const { gets, print } = require('./funcoes auxiliares_ex3');
+
+const salarioBruto = gets();
+const valorBeneficio = gets();
+
+function calcularPorcentagem(valor, porcentagem) {
+    return valor * (porcentagem / 100);
+}
+
+function pegarAliquota(salario) {
+    if (salario >= 0 && salario <= 1100) {
+        return 5;
+    } else if (salario > 1100 && salario <= 2500) {
+        return 10;
+    } else if (salario > 2500) {
+        return 15;
+    } else {
+        return 0;
+    }
+}
+
+const aliquotaImposto = pegarAliquota(salarioBruto);
+const valorImposto = calcularPorcentagem(salarioBruto,aliquotaImposto);
+
+const salarioLiquido = salarioBruto - valorImposto + valorBeneficio;
+
+print(salarioLiquido.toFixed(2));
